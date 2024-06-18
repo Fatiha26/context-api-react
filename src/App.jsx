8dd5f154e8fa3eb4dd43ae2402a693e7b3609e8d
Lamia Fatiha <f6lamia@gmail.com>
2024-06-18 12:14:38 +0600

    context api concept clear

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+import ComA from './ComA';
+
+const Firstname = createContext();
+
+const A
---
+++ src/ComC.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ComC = () => {
+ return <h1>My name is lllll</h1>
+};
+
+export default ComC;
